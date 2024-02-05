525ba28aa3c39a4bdf0d8f3e31d3354cd7edf3f3
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-02-05 16:09:43 +0000

    add search container component and css globals

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -5,14 +5,22 @@ import React from 'react';
 
 const FormContainer: React.FC = () => {
   return (
-    <Form.Root>
-      <Form.Field name="input">
+    <Form.Root className="w-11/12 flex flex-col m-auto gap-4 items-center ">
+      <Form.Field
+        name="username"
+        className="flex flex-col items-center text-center gap-2"
+      >
         <Form.Label />
-        <Form.Control />
-        <Form.Message match="valueMissing" />
-        Please insert a user name
+        Insert the name you are looking for
+        <Form.Control className="bordersContainers rounded-md px-1 py-1" />
+        <Form.Message match="valueMissing">
+          Please insert a user name
+        </Form.Message>
+        {/* <Form.ValidityState /> */}
       </Form.Field>
-      <Form.Submit>{/* <button>Search</button> */}</Form.Submit>
+      <Form.Submit className="spaceInText bordersContainers py-2 px-11 rounded-md bg-black text-white">
+        Submit
+      </Form.Submit>
     </Form.Root>
   );
 };
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
     <>
       <div className="flex justify-around items-center p-4 border-2 border-black bg-white">
         <div>
-          <h1 className="letter-spacing: 0.1em font-bold text-3xl md:text-5xl ">
+          <h1 className="spaceInText font-bold text-3xl md:text-5xl ">
             Well Github
           </h1>
           <h2>Search engine for Github users</h2>
