0f5b7910e85721736df9927820d6d500057faba2
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-02-05 12:17:58 +0000

    Add header into layout for all the pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Roboto_Mono } from 'next/font/google';
 import './globals.css';
 const roboto_mono = Roboto_Mono({ subsets: ['latin'] });
+import Header from './components/Header';
 
 export const metadata: Metadata = {
   title: 'WellGithub',
@@ -21,7 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto_mono.className}>{children}</body>
+      <body className={roboto_mono.className}>
+        {/* import Header for all the pages */}
+        <Header />
+
+        {children}
+      </body>
     </html>
   );
 }
