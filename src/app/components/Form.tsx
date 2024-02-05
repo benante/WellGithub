f8897a7a30fdde0b8eecd2647f39cc822e4728cc
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-02-05 16:28:30 +0000

    Add icon & add style to form

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -2,26 +2,31 @@
 
 import * as Form from '@radix-ui/react-form';
 import React from 'react';
+import Image from 'next/image';
+import icon from '../../../public/android-chrome-512x512.png';
 
 const FormContainer: React.FC = () => {
   return (
-    <Form.Root className="w-11/12 flex flex-col m-auto gap-4 items-center ">
-      <Form.Field
-        name="username"
-        className="flex flex-col items-center text-center gap-2"
-      >
-        <Form.Label />
-        Insert the name you are looking for
-        <Form.Control className="bordersContainers rounded-md px-1 py-1" />
-        <Form.Message match="valueMissing">
-          Please insert a user name
-        </Form.Message>
-        {/* <Form.ValidityState /> */}
-      </Form.Field>
-      <Form.Submit className="spaceInText bordersContainers py-2 px-11 rounded-md bg-black text-white">
-        Submit
-      </Form.Submit>
-    </Form.Root>
+    <>
+      <Form.Root className="w-11/12 flex flex-col m-auto gap-4 items-center my-8">
+        <Image alt="icon" src={icon} width={300}></Image>
+        <Form.Field
+          name="username"
+          className="flex flex-col items-center text-center gap-2"
+        >
+          <Form.Label />
+          Insert the name you are looking for
+          <Form.Control className="bordersContainers rounded-md px-1 py-1" />
+          <Form.Message match="valueMissing">
+            Please insert a user name
+          </Form.Message>
+          {/* <Form.ValidityState /> */}
+        </Form.Field>
+        <Form.Submit className="spaceInText bordersContainers py-2 px-11 rounded-md bg-black text-white">
+          Submit
+        </Form.Submit>
+      </Form.Root>
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-import Header from './components/Header';
 import FormContainer from './components/Form';
 
 export default function Home() {
