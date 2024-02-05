6f7e36d3c18e7d0f13b565469ac431a6f2edac8c
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-02-05 12:00:19 +0000

    Add ts to header, box sizing to globals

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import { FaGithub } from 'react-icons/fa';
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <div className="flex justify-around items-center p-4 border-2 border-black bg-white">
+        <div>
+          <h1 className="letter-spacing: 0.1em font-bold text-3xl md:text-5xl ">
+            Well Github
+          </h1>
+          <h2>Search engine for Github users</h2>
+        </div>
+        <Link href="https://github.com/" aria-label="GitHub Link">
+          <FaGithub size={80} />
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Header;
