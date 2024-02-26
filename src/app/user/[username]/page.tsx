a22a529aecfb6010495b7e57884173a480f26ea5
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-02-26 13:05:36 +0000

    Fetch data working

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -3,25 +3,73 @@
 import React, { useEffect, useState } from 'react';
 import fetchUser from '@/app/api/github';
 
+const mock = {
+  login: 'benante',
+  id: 63957194,
+  avatar_url: 'https://avatars.githubusercontent.com/u/63957194?v=4',
+};
+
+// console.log(mock);
+
+interface Info {
+  login: string;
+  id: number;
+  avatar_url: string | null;
+  company: string | null;
+  email: string | null;
+  followers: number;
+  following: number;
+  location: string | null;
+  public_repos: number | null;
+  url: string;
+}
+
 function Page({ params }: { params: { username: string } }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
-      const userData = await fetchUser(params.username);
-      setUser(userData);
+      const res = await fetchUser(params.username);
+      const {
+        login,
+        id,
+        avatar_url,
+        company,
+        email,
+        followers,
+        following,
+        location,
+        public_repos,
+        url,
+      } = res;
+      const userInfo: Info = {
+        login,
+        id,
+        avatar_url,
+        company,
+        email,
+        followers,
+        following,
+        location,
+        public_repos,
+        url,
+      };
+      setUser(userInfo);
     };
     fetchData();
-    console.log(user);
   }, []);
 
   return (
     <>
-      <div>Not working</div>
-      {user &&
-        Object.keys(user).map((e) => {
-          return <div key={e}>username is: {e}</div>;
-        })}
+      {user && (
+        <ul>
+          {Object.entries(user).map(([key, value]) => (
+            <li key={key}>
+              <strong>{key}</strong>: {value}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
