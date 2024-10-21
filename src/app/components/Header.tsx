40c8e4d2b2fdfb288b7002647302535aa6c6f67b
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-10-21 11:50:57 +0100

    Add Link to Header to auto-direct to homepage

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,9 +7,13 @@ const Header: React.FC = () => {
     <>
       <div className="flex justify-around items-center p-4 border-2 border-black bg-white">
         <div>
-          <h1 className="spaceInText font-bold text-3xl md:text-5xl ">
-            Well Github
-          </h1>
+          <Link href={'/'}>
+            <h1 className="spaceInText font-bold text-3xl md:text-5xl ">
+              {' '}
+              Well Github
+            </h1>
+          </Link>
+
           <h2>Search engine for Github users</h2>
         </div>
         <Link href="https://github.com/" aria-label="GitHub Link">
